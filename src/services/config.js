06b14c7a71e8188488d52a87088015d7d0f3f94b
Harshit Mathur <harshit.mathur@noitechnologies.com>
2023-06-02 10:52:23 +0530

    Initial Commit.

---
+++ src/services/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQmww4P2TaIbNJ1hWRxEYCz0wrqnVGuV8",
+  authDomain: "ecommerce-spex-181b4.firebaseapp.com",
+  projectId: "ecommerce-spex-181b4",
+  storageBucket: "ecommerce-spex-181b4.appspot.com",
+  messagingSenderId: "1033043602966",
+  appId: "1:1033043602966:web:56b83147722be836ad4f37"
+};  
+
+export default firebaseConfig;

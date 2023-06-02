664f88ef2c685f18256fbc78d2cbec76c76cb3ae
Harshit Mathur <harshit.mathur@noitechnologies.com>
2023-06-02 12:03:22 +0530

    Initial Commit.

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
         {/* <strong>
           <span>
             Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
+            <a href="https://github.com/harshitmathur007/react-demo-ecommerce">HERE</a>
           </span>
         </strong> */}
       </div>
